e6efe7456b4c4ea0c4ba91e2a513fd7ef57537f2
EvgenyDegtyarev <e.degtyarev@icloud.com>
2022-01-03 18:02:56 +0300

    lesson1

---
+++ GBShop-Q3UITests/GBShop_Q3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GBShop_Q3UITestsLaunchTests.swift
+//  GBShop-Q3UITests
+//
+//  Created by Евгений Дегтярев on 03.01.2022.
+//
+
+import XCTest
+
+class GBShop_Q3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
